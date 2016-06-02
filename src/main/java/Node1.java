2d03b0a55f201c7911e3b89049ed9325a7ff5a87
rhuanca <rhuanca@gmail.com>
2016-06-02 09:29:21 -0400

    added node 1

--- src/main/java/Node1.java
+++ src/main/java/Node1.java
@@ -2,9 +2,9 @@ import com.hazelcast.config.ClasspathXmlConfig;
 import com.hazelcast.config.Config;
 import com.hazelcast.core.Hazelcast;
 
-public class Node {
+public class Node1 {
 	public static void main(String args[]) {
-		Config cfg = new ClasspathXmlConfig("hazelcast-node.xml");
+		Config cfg = new ClasspathXmlConfig("hazelcast-node1.xml");
 		Hazelcast.newHazelcastInstance(cfg);
 	}
 }
